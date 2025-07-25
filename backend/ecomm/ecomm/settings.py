f5631aabf08c63506070f6e5ff403d151baf0804
Suhaas22 <bommadevarasuhaas7@gmail.com>
2025-07-25 16:57:06 +0530

    added print stat in settings

--- backend/ecomm/ecomm/settings.py
+++ backend/ecomm/ecomm/settings.py
@@ -38,6 +38,7 @@ DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 
+print(">>> ALLOWED_HOSTS =", ALLOWED_HOSTS)
 
 # Application definition
 
