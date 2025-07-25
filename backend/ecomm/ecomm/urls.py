6f36a2b168aa5f3561c54ba7c6cf3fdd865af6ff
Suhaas22 <bommadevarasuhaas7@gmail.com>
2025-07-25 17:02:40 +0530

    chore: added root route for backend health check

--- backend/ecomm/ecomm/urls.py
+++ backend/ecomm/ecomm/urls.py
@@ -18,8 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse
+
+def home(request):
+    return JsonResponse({"message": "Django backend is live 🚀"})
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('api/', include('ecommapp.urls')), 
 ]
